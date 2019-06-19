23c801b91ad7174418f2d41d762c2e0e70105e0c
Arturo Nereu <hdgam3r@gmail.com>
2019-06-18 21:02:24 -0500

    Added Instantiation via Addressables API

---
+++ 01_BasicDemo/Assets/Scripts/CharacterManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets; //TODO: Mention the use of this namespace
+using UnityEngine.ResourceManagement.AsyncOperations; // TODO: Mention that this is needed to do the async operations over the lists?
+
+public class CharacterManager : MonoBehaviour
+{
+    //public GameObject archerObject;
+
+    public AssetReference m_ArcherObject;
+
+    public List<AssetReference> m_Characters;
+
+    bool m_AssetsReady = false;
+    int m_ToLoadCount;
+    int m_CharacterIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_ToLoadCount = m_Characters.Count;
+
+        foreach(var character in m_Characters)
+        {
+            character.LoadAssetAsync<GameObject>().Completed += OnCharacterAssetLoaded;
+        }
+    }
+
+
+    public void SpawnCharacter(int characterType)
+    {
+
+        if (characterType == 0)
+        {
+            //Instantiate(archerObject);
+            m_ArcherObject.InstantiateAsync();
+        }
+    }
+
+    public void SpawnCharacter()
+    {
+        if(m_AssetsReady)
+        {
+            Instantiate(m_Characters[m_CharacterIndex].Asset);
+
+            m_CharacterIndex++;
+            if (m_CharacterIndex >= m_Characters.Count)
+            {
+                m_CharacterIndex = 0;
+            }
+        }
+    }
+
+    private void OnDestroy() //TODO: Should we teach instantiate with game objects and then manually release?
+    {
+        foreach(var character in m_Characters)
+        {
+            character.ReleaseAsset();
+        }
+    }
+
+    void OnCharacterAssetLoaded(AsyncOperationHandle<GameObject> obj)
+    {
+        m_ToLoadCount--;
+
+        if (m_ToLoadCount <= 0)
+            m_AssetsReady = true;
+    }
+}
---
+++ 01_BasicDemo/Assets/Scripts/TowerManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.ResourceManagement.ResourceLocations;
+
+public class TowerManager : MonoBehaviour
+{
+    public List<IResourceLocation> m_Characters;
+
+    public AssetLabelReference towerLabel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Addressables.DownloadDependenciesAsync(towerLabel).Completed += FinishedLoadingTowers;
+        Addressables.LoadAssetsAsync<IResourceLocation>(new List<object> { towerLabel.labelString }, null, Addressables.MergeMode.Intersection).Completed += FinishedLoadingTowers2;
+    }
+
+    void FinishedLoadingTowers2(AsyncOperationHandle<IList<IResourceLocation>> op)
+    {
+        foreach(var tower in op.Result)
+        {
+            Addressables.InstantiateAsync(tower);
+        }
+    }
+
+    //void FinishedLoadingTowers(AsyncOperationHandle obj)
+    //{
+    //    Debug.Log("Loaded Towers");
+    //    m_Characters = new List<IResourceLocation>(obj.Result);
+    //}
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
