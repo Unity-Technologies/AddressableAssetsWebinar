c1af3fd251d7135f69d4cb30de866281be566198
Arturo Nereu <hdgam3r@gmail.com>
2019-06-23 18:43:43 -0500

    Restored CharacterManager.cs script to an initial state

--- UnityRoyale_Start/Assets/Scripts/CharacterManager.cs
+++ UnityRoyale_Start/Assets/Scripts/CharacterManager.cs
@@ -5,48 +5,49 @@ using UnityEngine.ResourceManagement.AsyncOperations; // TODO: Mention that this
 
 public class CharacterManager : MonoBehaviour
 {
-    //public GameObject m_archerObject;
+    public GameObject m_archerObject;
 
-    public AssetReference m_ArcherObject;
+    //public AssetReference m_ArcherObject;
 
-    public List<AssetReference> m_Characters;
-    bool m_AssetsReady = false;
-    int m_ToLoadCount;
-    int m_CharacterIndex = 0;
+    //public List<AssetReference> m_Characters;
+    //bool m_AssetsReady = false;
+    //int m_ToLoadCount;
+    //int m_CharacterIndex = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        m_ToLoadCount = m_Characters.Count;
+        //m_ToLoadCount = m_Characters.Count;
 
-        foreach (var character in m_Characters)
-        {
-            character.LoadAssetAsync<GameObject>().Completed += OnCharacterAssetLoaded;
-        }
+        //foreach (var character in m_Characters)
+        //{
+        //    character.LoadAssetAsync<GameObject>().Completed += OnCharacterAssetLoaded;
+        //}
     }
 
 
     public void SpawnCharacter(int characterType)
     {
-        //Instantiate(m_ArcherObject);
+        Instantiate(m_archerObject);
+        
         //m_ArcherObject.InstantiateAsync();
  
-        m_Characters[characterType].InstantiateAsync();
+        //m_Characters[characterType].InstantiateAsync();
     }
 
-    void OnCharacterAssetLoaded(AsyncOperationHandle<GameObject> obj)
-    {
-        m_ToLoadCount--;
+    //void OnCharacterAssetLoaded(AsyncOperationHandle<GameObject> obj)
+    //{
+    //    m_ToLoadCount--;
 
-        if (m_ToLoadCount <= 0)
-            m_AssetsReady = true;
-    }
+    //    if (m_ToLoadCount <= 0)
+    //        m_AssetsReady = true;
+    //}
 
-    private void OnDestroy() //TODO: Should we teach instantiate with game objects and then manually release?
-    {
-        foreach (var character in m_Characters)
-        {
-            character.ReleaseAsset();
-        }
-    }
+    //private void OnDestroy() //TODO: Should we teach instantiate with game objects and then manually release?
+    //{
+    //    foreach (var character in m_Characters)
+    //    {
+    //        character.ReleaseAsset();
+    //    }
+    //}
 }
