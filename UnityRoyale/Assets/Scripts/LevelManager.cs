8645ca5bcbb8c35b5ada3b81da05a33747856dc1
Arturo Nereu <hdgam3r@gmail.com>
2020-02-25 13:59:54 -0600

    UCD Demo with seasonal background completed

---
+++ UnityRoyale/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+public class LevelManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Addressables.InstantiateAsync("Level1");
+    }
+}
